15e3a7eeac490d69fc0fc3fbff8188dcc5be99eb
Daffa Akbar <daffaputradamar@gmail.com>
2019-10-08 15:48:05 +0700

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { BerandaComponent } from "./pages/beranda/beranda.component";
+import { LoginComponent } from "./pages/login/login.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: BerandaComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BerandaComponent } from './pages/beranda/beranda.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BerandaComponent,
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
